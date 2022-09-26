ff882d3c2e77693327d9ff7ab1f99caec66041ac
mrgallant101 <sanisunday20@gmail.com>
2022-09-26 10:48:57 +0100

    Debugged bank.py project

---
+++ bank.py
@@ -0,0 +1,101 @@
+"""
+This assessment is to test your knowledge of python dictionaries and functions and a lot more
+
+- I used some advanced string formatting in the code below, check them out on Google to understand how the work and try
+to use them in your code some other time
+- This code is a demo bank application
+- The dictionary stores users and bank officials information like password and users account balance
+- You need to understand how the dictionary is structured before attempting to debug this code
+- The code below checks if  a user is logging in with the right information/details
+- And shows the user balance
+- A bank official can also log in
+
+ +++ Hint
+ Run the code first before attempting to debug
+Happy coding and debugging !!!
+
+"""
+print("""
+                        ░░░░                      
+                    ░░░░  ░░░░                    
+                    ░░░░░░░░░░                    
+                        ░░░░                      
+
+          ▒▒▒▒    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒                  
+          ▒▒░░▒▒▒▒░░░░░░░░░░░░░░▒▒▒▒              
+          ▒▒▒▒▒▒░░░░░░▒▒▒▒▒▒▒▒░░░░░░▒▒            
+        ▒▒░░░░░░░░▒▒▒▒░░░░░░▒▒▒▒░░░░▒▒            
+        ▒▒░░██░░░░░░░░░░░░░░░░░░░░░░░░▒▒          
+    ▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒        
+    ▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒  ▒▒    
+    ▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒      
+      ▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒          
+        ▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒          
+          ▒▒▒▒░░░░░░░░░░░░░░░░░░░░▒▒▒▒            
+            ▒▒▒▒▒▒░░░░░░░░░░░░▒▒▒▒▒▒              
+            ▒▒░░░░▒▒▒▒▒▒▒▒▒▒░░░░░░▒▒              
+            ▒▒▒▒▒▒            ▒▒▒▒▒▒              
+
+
+██████████████████████████████████████████████████
+""")
+bank_model = {
+    "officials": {
+        # you can choose to change name afterwards
+        0: {
+            "name": "Danile Vanek",
+            "password": "dv000"
+        },
+        1: {
+            "name": "Donald Dorcas",
+            "password": "dd001"
+        }
+    },
+    "customers": {
+        "0123456789": {
+            "account_type": "current",
+            "account_name": "John Kennedy",
+            "balance": 250,
+            "password": "jk002"
+        },
+        "0444456789": {
+            "account_type": "current",
+            "account_name": "richard james",
+            "balance": 1250,
+            "password": "rj003"
+        },
+    }
+}
+
+
+def login():
+    print("Welcome to TEEKAY Bank")
+    user = int(input("Log in as a bank official or a bank user? Enter 1. for official 2. for user "))
+
+    if user == 1:
+        id = int(input("Enter your official id: "))
+        password = input("Enter your official password: ")
+
+        if password == bank_model["officials"][id]["password"]:
+            msg = "Welcome back %s " % (bank_model["officials"][id]["name"])
+            print(msg)
+
+        elif password != bank_model["officials"][id]["password"]:
+
+            print("password or id is invalid!!")
+            login()
+    else:
+        id = input("Enter your account number: ")
+        password = input("Enter your account password: ")
+
+        if password == bank_model["customers"][id]["password"]:
+            msg = "Welcome back %s\nYour current balance: $%s" % (bank_model["customers"][id]["account_name"],
+                                                                  bank_model['customers'][id]["balance"])
+            print(msg)
+
+        elif password != bank_model["customers"][id]["password"]:
+            print("password or id is invalid!!")
+            login()
+
+
+login()
